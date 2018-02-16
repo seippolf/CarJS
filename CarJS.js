050bc62365d41b38d1866bbfdfadd852930f1a2a
seippolf <justin@rowajo.com>
2018-02-16 14:44:05 -0800

    Sixth commit

--- CarJS.js
+++ CarJS.js
@@ -3,5 +3,19 @@ function airflowActual() {
     var af = document.getElementById('afRatio').value;
     var bsfc = document.getElementById('bsFuelConsumption').value; 
     var wa = hp * af * (bsfc/60);
-    alert(wa);
+    document.getElementById('airflowCalc').innerHTML = "Airflowactual: " + wa;
+    return wa;
+}
+function mapRequired() {
+    var wa = airflowActual();
+    var r = 639.6;
+    var tm = 130;
+    var ve = 0.98;
+    var n = 3300;
+    var vd = 400;
+    var mp = (wa * r * (460 + tm)) / (ve * (n/2) * vd)
+    if(ve <= 0 || n <= 0 || vd <= 0) {
+        alert("Less than zero... this is impossible.");
+    }
+    alert(mp);
 }
