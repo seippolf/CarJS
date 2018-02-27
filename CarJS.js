6572a7ff065cea0a9000574320c45b9ebaf6211b
seippolf <justin@rowajo.com>
2018-02-27 08:47:13 -0800

    Eighth Commit

--- CarJS.js
+++ CarJS.js
@@ -15,15 +15,35 @@ function mapRequired() {
     var vd = document.getElementById('volDisplacement').value;
     var tempSelect = document.getElementById('tempUnit').value;
     if(tempSelect == 'celsius') {
-        tm = (tm * 1.8) + 32;
+        tm = toFahrenheit(tm);
     }
-    var volSelect = document.getElementById().value;
+    var volSelect = document.getElementById('volUnit').value;
+    if(volSelect == 'cubicCentimeters') {
+        vd = ccImperial(vd);
+    }
+    else if(volSelect == 'liters') {
+        vd = literImperial(vd);
+    }
     var mp = (wa * r * (460 + tm)) / (ve * (n/2) * vd)
     if(ve <= 0 || n <= 0 || vd <= 0) {
+    //If this information was used... I fear what would happen. That's why it isn't shown.
         alert("Less than zero... this is impossible.");
-        document.getElementById('mapReq').innerHTML = "Manifold Pressure Requirements: NaN";
+        document.getElementById('mapReq').innerHTML = "Manifold Pressure Requirements: NaN"; 
     }
     else {
         document.getElementById('mapReq').innerHTML = "Manifold Pressure Requirements: " + mp; 
     }
+}
+//Garrett uses the Imperial System... 
+function toFahrenheit(celsiusValue) { 
+    var fahrValue = (celsiusValue * 1.8) + 32;
+    return fahrValue;
+}
+function ccImperial(ccValue) {
+    var ciValue = ccValue * 0.0610237;
+    return ciValue;
+}
+function literImperial(literValue) {
+    var ciValue = literValue * 61.0237;
+    return ciValue;
 }
