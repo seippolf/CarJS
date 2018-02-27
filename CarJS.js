ec34c7abacab51da9a7c928e7e123e84d25f774b
seippolf <justin@rowajo.com>
2018-02-27 08:12:45 -0800

    Seventh Commit -WIP

--- CarJS.js
+++ CarJS.js
@@ -9,13 +9,21 @@ function airflowActual() {
 function mapRequired() {
     var wa = airflowActual();
     var r = 639.6;
-    var tm = 130;
-    var ve = 0.98;
-    var n = 3300;
-    var vd = 400;
+    var tm = document.getElementById('manTemperature').value;
+    var ve = document.getElementById('volEfficiency').value;
+    var n = document.getElementById('engineSpeed').value;
+    var vd = document.getElementById('volDisplacement').value;
+    var tempSelect = document.getElementById('tempUnit').value;
+    if(tempSelect == 'celsius') {
+        tm = (tm * 1.8) + 32;
+    }
+    var volSelect = document.getElementById().value;
     var mp = (wa * r * (460 + tm)) / (ve * (n/2) * vd)
     if(ve <= 0 || n <= 0 || vd <= 0) {
         alert("Less than zero... this is impossible.");
+        document.getElementById('mapReq').innerHTML = "Manifold Pressure Requirements: NaN";
     }
-    alert(mp);
+    else {
+        document.getElementById('mapReq').innerHTML = "Manifold Pressure Requirements: " + mp; 
+    }
 }
